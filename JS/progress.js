a84f109772a2a05340609d66b4613df8609b3daa
Betterteng <betterteng@gmail.com>
2018-06-03 11:48:06 +1000

    Text Area can be changed now...

--- JS/progress.js
+++ JS/progress.js
@@ -4,9 +4,26 @@
 
 var storyIndex = 0;
 
+// Add an click listener to the progressBtn
 document.getElementById("progressBtn").addEventListener("click", tellTheStory);
+// Add an click listener to the resetBtn
+document.getElementById("resetBtn").addEventListener("click", resetTheStory);
 
+// Change the layout of the website...
 function tellTheStory() {
-    document.getElementById("storyTellArea").innerHTML = "<br>" + storyArray[storyIndex];
-    storyIndex++;
+    changeText()
 }
+
+// Change the layout to the original status...
+function resetTheStory() {
+    storyIndex = 0;
+    document.getElementById("storyTellArea").innerHTML = "<br>" + originalText;
+}
+
+function changeText() {
+    document.getElementById("storyTellArea").innerHTML = "<br>" + storyArray[storyIndex];
+    if (storyIndex < storyArray.length - 1) {
+        storyIndex++;
+    }
+}
+
